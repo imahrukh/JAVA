cddfd74fd21c39a99ea8b515769365f0bb20cbe6
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 13:00:01 +0500

    compile on cmd

---
+++ Print/Compiling.java
@@ -0,0 +1,15 @@
+public class Compiling {
+  public static void main(String[] args) {
+    
+    System.out.println("Java is a class-based language.");
+    System.out.println("Java classes have a 'main' method.");
+    System.out.println("Java statements end with a semicolon.");
+
+    System.out.println("Programming is... fun!");
+    
+  }
+}
+// to compile and execute the file in terminal open the directory
+// javac filename.java
+//java filename
+//if any error found type ls(short for "list");it'll open available files
