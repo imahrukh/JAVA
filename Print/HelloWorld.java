3b6b01e3aa84bc0aeea64f1d48f7f31692685ce7
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 12:32:50 +0500

    Created HelloWorld.java

---
+++ Print/HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{ //public class should be same as file's name
+    public static void main(String args[]){ //entry point for JVM
+        //public: access specifier
+        //static : doesn't belong to an object
+        //void: return type
+        //args[]: array
+        System.out.println("Hello World");//System: built-in class -> out:method for output,
+    } //println(): print data on screen and moves to next Link
+      //print(): print data on screen and stays on same line
+}
