289a09907757e14e51d06716c44d88a6154ae383
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 19:45:58 +0500

    final keyword

---
+++ ArithmeticOperations/Final.java
@@ -0,0 +1,8 @@
+public class Final {
+	public static void main(String[] args) { 
+      final double pi = 3.14;
+      //final : keyword , constant value
+      System.out.println("the final value of pi: "+pi);
+      //pi +=2;
+	}
+}
