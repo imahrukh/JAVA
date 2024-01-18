41219df32070b5fd922fda00679f1679e9dac130
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 13:58:24 +0500

    review

---
+++ Variables/MyProfile.java
@@ -0,0 +1,14 @@
+public class MyProfile {
+	public static void main(String[] args) {   
+    String name ="Mah Rukh";
+    int age = 19;
+    double salary = 2598.75;
+    char gender = 'f';
+    boolean lookingForJob = true;
+    System.out.println("Name: "+ name);
+    System.out.println("Age: "+ age);
+    System.out.println("salary"+ salary);
+    System.out.println("Gander "+ gender);
+    System.out.println("Looking for job "+ lookingForJob);
+	}
+}
