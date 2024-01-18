0834a2b4f3e9244c0b213f3a9c7352dfc8c44c3a
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 12:35:20 +0500

    Create HideAndSeek.java

---
+++ Print/HideAndSeek.java
@@ -0,0 +1,11 @@
+public class HideAndSeek {
+  public static void main(String[] args) {
+    System.out.println("Let's play hide and seek.");
+    //println(): print line and moves to the next line
+    System.out.print("Three...");
+    //prints line and stays on same line
+    System.out.print("Two...");
+    System.out.println("One...");
+    System.out.println("Ready or not, here I come!");
+  }
+}
