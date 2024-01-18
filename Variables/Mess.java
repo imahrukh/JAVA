dacec6d043c9d0a26613182be7def5b6bd7fcd1e
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 13:49:23 +0500

    data types

---
+++ Variables/Mess.java
@@ -0,0 +1,14 @@
+public class Mess {
+	public static void main(String[] args) {   
+		int year = 2001;
+    String title = "Shrek";
+    char genre = 'C';
+    double runtime = 1.58;
+    boolean isPG = true;
+    System.out.println("Year: "+year);
+    System.out.println("Title: "+title);
+    System.out.println("Genre: "+genre);
+    System.out.println("Runtime: "+runtime);
+     System.out.println("isPG: "+isPG);
+	}
+}
