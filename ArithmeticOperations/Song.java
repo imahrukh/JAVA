a2c5b33442e2f9d954f720d54558fe3b82d9faf2
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-18 19:36:10 +0500

    .equals() method

---
+++ ArithmeticOperations/Song.java
@@ -0,0 +1,9 @@
+public class Song {
+  	public static void main(String[] args){
+      String line1 = "Nah nah nah nah nah nah nah nah nah yeah";
+      String line2 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      String line3 = "Nah nah nah nah nah nah, nah nah nah, hey Jude";
+      System.out.println("Line 1 is equal to Line 2: "+ line1.equals(line2));
+       System.out.println("Line 2 is equal to Line 3: "+ line2.equals(line3));
+    }       
+}
