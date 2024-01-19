39595272d238f366daefc965b0cc562f9551ce50
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-19 20:34:10 +0500

    Updated Store

--- OOP/Store.java
+++ OOP/Store.java
@@ -1,17 +1,20 @@
 public class Store {
-  
-  // new method: constructor!
-  public Store() {
-    System.out.println("I am inside the constructor method.");
+  // instance fields
+  String productType;
+  int inventoryCount;
+  double inventoryPrice;
+  // constructor method
+  public Store(String product, int count, double price) {
+    productType = product;
+    inventoryCount = count;
+    inventoryPrice= price;
   }
   
-  // main method is where we create instances!
+  // main method
   public static void main(String[] args) {
-    System.out.println("Start of the main method.");
-    // create the instance below
-    Store lemonadeStand = new Store();
-    // print the instance below
-    System.out.println(lemonadeStand);
-    //returns garbage value because no value was asigned initially
+    Store cookieShop = new Store("cookies", 12, 3.75);
+    System.out.println("Product Type: "+ cookieShop.productType);
+    System.out.println("Product Count: "+ cookieShop.inventoryCount);
+    System.out.println("Product Price: "+ cookieShop.inventoryPrice);
   }
 }
