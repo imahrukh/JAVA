4c17c15401d663c32e97bbf751d6f4842a447ddc
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-19 23:33:10 +0500

    using methods

---
+++ OOP/SavingsAccount.java
@@ -0,0 +1,47 @@
+public class SavingsAccount {
+  
+  int balance;
+  
+  public SavingsAccount(int initialBalance){
+    balance = initialBalance;
+  }
+
+  public void checkBalance(){
+    System.out.println("Hello!\nYour balance is "+ balance);
+  }
+  
+  public void deposit(int amountToDeposit){
+    balance+=amountToDeposit;
+    System.out.println("You just deposited "+ amountToDeposit);
+  }
+
+  public int withdraw(int amountToWithdraw){
+    balance-=amountToWithdraw;
+    System.out.println("You just withdrew "+ amountToWithdraw);
+    return amountToWithdraw;
+  }
+  public static void main(String[] args){
+    SavingsAccount savings = new SavingsAccount(2000);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Withdrawing:
+    savings.withdraw(300);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Deposit:
+   savings.deposit(600);
+    
+    //Check balance:
+    savings.checkBalance();
+    
+    //Deposit:
+    savings.deposit(600);
+    
+    //Check balance:
+    savings.checkBalance();
+  }       
+}
