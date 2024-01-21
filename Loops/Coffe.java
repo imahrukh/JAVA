ca2423f8f040493e598cccd368c9321c7c2986e7
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-21 22:23:20 +0500

    for loop

---
+++ Loops/Coffe.java
@@ -0,0 +1,15 @@
+class Coffe {
+  
+  public static void main(String[] args) {
+    
+    //int cupsOfCoffee = 1;
+    
+    for(int cupsOfCoffee =1;cupsOfCoffee <= 100;cupsOfCoffee++) {
+      
+      System.out.println("Fry drinks cup of coffee #" + cupsOfCoffee);
+      
+    }
+    
+  }
+  
+}
