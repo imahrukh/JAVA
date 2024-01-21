01da76912cc373379e45e9bb5f1093180912d9a1
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-21 20:22:39 +0500

    .add()

---
+++ ArrayList/ToDos.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+class ToDos {
+    
+  public static void main(String[] args) {
+    
+    ArrayList<String> toDoList = new ArrayList<String>();
+    String toDo1 = "Water plants";
+    String toDo2 = "Aquamarine plants";
+    String toDo3 = "Terrestrial plants";
+    toDoList.add(toDo1);
+    toDoList.add(toDo2);
+    toDoList.add(toDo3);
+    System.out.println(toDoList);
+    
+  }
+  
+}
