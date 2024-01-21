0b7b65cad661be30916ff659fe175fea9215f063
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-21 20:25:18 +0500

    .add() method

--- ArrayList/ToDos.java
+++ ArrayList/ToDos.java
@@ -12,7 +12,10 @@ class ToDos {
     toDoList.add(toDo2);
     toDoList.add(toDo3);
     System.out.println(toDoList);
-    
+    toDoList.add(1,toDo1);
+    toDoList.add(3,toDo2);
+    toDoList.add(2,toDo3);
+    System.out.println(toDoList);
   }
   
 }
