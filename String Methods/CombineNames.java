b184a2fcc7db2ad15777983bf66fb16b760d8624
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-22 17:14:50 +0500

    concatination

---
+++ String Methods/CombineNames.java
@@ -0,0 +1,10 @@
+public class CombineNames {
+  
+	public static void main(String[] args) {
+    
+    String firstName = "Mah";  
+    String lastName = "Rukh";
+
+    System.out.println(firstName.concat(lastName));
+  }
+}
