cc0c3b8a658ad5ce90b99d3059237d38c94d7a10
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-22 20:46:03 +0500

    uppercase/lowercase

---
+++ String Methods/Hashtag.java
@@ -0,0 +1,11 @@
+public class Hashtag {
+  
+  public static void main(String[] args) {
+    
+    String hashtag = "#100DaysOfCode";
+    String upper = hashtag.toUpperCase();
+    String lower = hashtag.toLowerCase();
+    System.out.println("Lower case: "+lower);
+    System.out.println("Upper case: "+upper);
+  }  
+}
