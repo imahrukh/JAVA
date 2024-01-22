ebd1f49d15d4f12309d8f1211b73199afa995c06
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-22 20:09:40 +0500

    indexOf()

---
+++ String Methods/MorseCode.java
@@ -0,0 +1,9 @@
+public class MorseCode {
+  
+  public static void main(String[] args) {
+    
+    String code = ".... .. / .. .----. -- / ... --- -. -. -.-- / .- -. -.. / .. / .-.. .. -.- . / - --- / . .- - / .-. .- -- . -.";
+
+    System.out.println("The index of . .- - is: "+code.indexOf(". .- -"));
+  }
+}
