4f0bc15b65f96f86ed157e128761905c7bb947f4
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-22 20:40:05 +0500

    substring()

---
+++ String Methods/Poetry.java
@@ -0,0 +1,8 @@
+public class Poetry {
+  
+	public static void main(String[] args) {
+      
+    String line = "The Heav'ns and all the Constellations rung";     
+    System.out.println(line.substring(4, 11)); 
+  } 
+}
