a0f5c21a038b2d968feb55735992cef1b292b543
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-22 20:27:24 +0500

    charAt(index)

---
+++ String Methods/Initials.java
@@ -0,0 +1,11 @@
+public class Initials {
+  
+	public static void main(String[] args) {
+    
+    String firstName = "Mah";  
+    String lastName = "Rukh";
+    System.out.println("The initials: ");
+    System.out.println(firstName.charAt(0));
+    System.out.println(lastName.charAt(0)); 
+  } 
+}
