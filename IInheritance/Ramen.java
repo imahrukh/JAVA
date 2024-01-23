06ba00eba5b51f15fff008f636eefbbce9a6cee3
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-23 22:43:14 +0500

    child class to Noodless

---
+++ IInheritance/Ramen.java
@@ -0,0 +1,15 @@
+public class Ramen extends Noodless {
+  
+  Ramen() {
+    
+    super(30.0, 0.3, "flat", "wheat flour");
+    
+  }
+  
+ /* public boolean isTasty() {
+    
+    return false;
+    
+  }*/
+  
+}
