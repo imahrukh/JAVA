8337dc09e938ef77763ff30adf4bdd2b3ac6ed4f
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-23 19:14:14 +0500

    added static methods

--- OOP/ATM.java
+++ OOP/ATM.java
@@ -1,43 +1,39 @@
 public class ATM{
-
   public static int totalMoney = 0;
   public static int numATMs = 0;
-
   public int money;
 
   public ATM(int inputMoney){
     this.money = inputMoney;
-    totalMoney+=inputMoney;
-    numATMs ++;
+    numATMs += 1;
+    totalMoney += inputMoney;
   }
 
   public void withdrawMoney(int amountToWithdraw){
     if(amountToWithdraw <= this.money){
       this.money -= amountToWithdraw;
-      totalMoney-= amountToWithdraw;
+      totalMoney -= amountToWithdraw;
     }
   }
-
+  public static void averageMoney(){
+    System.out.println("Average money: "+ totalMoney/numATMs);
+    //System.out.println(this.money);
+    //nonstatic instances not allowed in static functions
+  }
+  
   public static void main(String[] args){
 
     System.out.println("Total number of ATMs: " + ATM.numATMs); 
     ATM firstATM = new ATM(1000);
-    System.out.println("Money in first ATM:  "+ firstATM.money);
     ATM secondATM = new ATM(500);
-    System.out.println("Money in second ATM:  "+ secondATM.money);
     System.out.println("Total number of ATMs: " + ATM.numATMs); 
 
     System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);  
-
     firstATM.withdrawMoney(500);
-    System.out.println("Money in first ATM:  "+ firstATM.money);
-    System.out.println("Money in second ATM:  "+ secondATM.money);
-    System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney); 
     secondATM.withdrawMoney(200);
-
-    System.out.println("Money in first ATM:  "+ firstATM.money);
-    System.out.println("Money in second ATM:  "+ secondATM.money);
     System.out.println("Total amount of money in all ATMs: " + ATM.totalMoney);    
+
+   ATM.averageMoney();
 
   }
 
