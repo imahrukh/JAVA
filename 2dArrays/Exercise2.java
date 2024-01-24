7a3e1e3f8a6aeb97cb590df4dfc50e7eec906514
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-24 18:40:10 +0500

    declare 2d static arrays

---
+++ 2dArrays/Exercise2.java
@@ -0,0 +1,14 @@
+public class Exercise2 {
+	public static void main(String[] args) {
+    
+    float[][] floatTwoD;
+    floatTwoD = new float [4][10];
+
+		int[][] dataChart = new int [15][8];
+
+    char[][] ticTacToe =  {{'X','O','O'},{'O','X',' '},{'X',' ','X'}};
+		//updating using initializer list such that o wins
+    ticTacToe = new char[][]{{'O','X','X'},{'X','O',' '},{'O',' ','O'}};
+
+    }
+}
