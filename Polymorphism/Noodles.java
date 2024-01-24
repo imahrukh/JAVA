ed0732e691f4c5291610eef4fa1d2518b1a4caa4
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-24 10:56:54 +0500

    child classes as parameter

---
+++ Polymorphism/Noodles.java
@@ -0,0 +1,22 @@
+class Noodles {
+  
+  protected double lengthInCentimeters;
+  protected double widthInCentimeters;
+  protected String shape;
+  protected String ingredients;
+  protected String texture = "brittle";
+  
+  Noodles(double lenInCent, double wthInCent, String shp, String ingr) {
+    
+    this.lengthInCentimeters = lenInCent;
+    this.widthInCentimeters = wthInCent;
+    this.shape = shp;
+    this.ingredients = ingr;
+    
+  }
+  
+  public String getCookPrep() {
+    
+    return "Boil noodle for 7 minutes and add sauce."; 
+  }
+}
