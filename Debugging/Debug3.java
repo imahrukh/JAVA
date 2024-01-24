486c2818b1f47be1ea9b967ab6f8848978b3b0bc
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-24 11:17:38 +0500

    logical errors

---
+++ Debugging/Debug3.java
@@ -0,0 +1,15 @@
+public class Debug3 {
+
+  public static void main(String[] args) {
+    
+    int steps = 10;
+    //for(int i =0;i<=step;i++)
+    for (int i = 1; i <= steps; i++) {
+      
+      System.out.println("Step #" + i);
+      
+    }
+    
+  }
+  
+}
