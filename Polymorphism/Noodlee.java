3d62eeead86b64c69715f6f2abb2ba79ee56ad3f
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-24 10:27:53 +0500

    using child class as its parent class

---
+++ Polymorphism/Noodlee.java
@@ -0,0 +1,24 @@
+class Noodlee {
+  
+  protected double lengthInCentimeters;
+  protected double widthInCentimeters;
+  protected String shape;
+  protected String ingredients;
+  protected String texture = "brittle";
+  
+  Noodlee(double lenInCent, double wthInCent, String shp, String ingr) {
+    
+    this.lengthInCentimeters = lenInCent;
+    this.widthInCentimeters = wthInCent;
+    this.shape = shp;
+    this.ingredients = ingr;
+    
+  }
+  
+  public void cook() {
+    
+    this.texture = "cooked";
+    
+  }
+  
+}
