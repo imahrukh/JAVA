a67ffa7fdfddda42df4b3904022c847340ab34dd
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 20:59:55 +0500

    back lash escapse characters in string

---
+++ String Methods/BackLashChar.java
@@ -0,0 +1,11 @@
+public class BackLashChar {
+  public static void main(String[] args) {
+  	System.out.println("Backlash exacpe characters");
+    String txt = "We are the so-called \"Vikings\" from the north.";
+    String txt1 = "It\'s alright.";
+    String txt2 = "The character \\ is called backslash.";
+    System.out.println(" \" quote :" +txt);
+    System.out.println("\' single quote: "+txt1);
+    System.out.println("\\ single backlash: "+txt2);
+  }
+}
