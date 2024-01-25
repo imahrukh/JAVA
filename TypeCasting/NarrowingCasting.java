045404e640fab2a562acbf5f298e333cb7d7eee6
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 20:41:53 +0500

    larger to smaller data type

---
+++ TypeCasting/NarrowingCasting.java
@@ -0,0 +1,10 @@
+//Narrowing Casting: conversion from larger type to smaller size
+public class NarrowingCasting {
+  public static void main(String[] args) {
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble; // Manual casting: double to int
+
+    System.out.println(myDouble);   // Outputs 9.78
+    System.out.println(myInt);      // Outputs 9
+  }
+}
