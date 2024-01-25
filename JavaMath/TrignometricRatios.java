072780f8af5bc62ee7ae7eb98ce9c4296bbe35fe
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 21:51:54 +0500

    trignometric ratios in math

---
+++ JavaMath/TrignometricRatios.java
@@ -0,0 +1,22 @@
+public class TrignometricRatios{
+  public static void main(String[] args) {
+  	System.out.println("arc cosine of 0 in radians: ");
+    System.out.println(Math.acos(0)); 
+    System.out.println("arc sine of 1 in radians: ");
+    System.out.println(Math.asin(1));  
+    System.out.println("arc tanget of -1 in radians: ");
+    System.out.println(Math.atan(-1));
+    System.out.println("sine of 30 in radians");
+    System.out.println(Math.sin(30));
+    System.out.println("cosine of 30 in radians");
+    System.out.println(Math.cos(30));
+    System.out.println("tangent of 30 in radians");
+    System.out.println(Math.tan(30));
+    System.out.println("sin h of 45 in radians");
+    System.out.println(Math.sinh(45));
+    System.out.println("cosine h of 45 in radians");
+    System.out.println(Math.cosh(45));
+    System.out.println("tangent h of 45 in radians");
+    System.out.println(Math.tanh(45));
+  }
+}
