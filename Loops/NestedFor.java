22ccccce13910c8aabef8910389d931c7ca18689
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 22:16:54 +0500

    nested for loops

---
+++ Loops/NestedFor.java
@@ -0,0 +1,11 @@
+public class NestedFor {
+  public static void main(String[] args) {
+    for (int i = 1; i <= 2; i++) {
+      System.out.println("Outer: " + i); // Executes 2 times
+   
+      for (int j = 1; j <= 3; j++) {
+        System.out.println(" Inner: " + j); // Executes 6 times (2 * 3)
+      }
+    } 
+  }
+}
