b2b90da2c35f1f89253f93e877e4946a60d51e24
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 22:01:50 +0500

    different methods in math

---
+++ JavaMath/Misc.java
@@ -0,0 +1,20 @@
+public class Misc{
+  public static void main(String[] args){
+    System.out.println("Cube root of 256: ");
+    System.out.println(Math.cbrt(256)); 
+    System.out.println("rounded value of 5.487: ");
+    System.out.println(Math.ceil(5.487));  
+    System.out.println("exponent of 4: ");
+    System.out.println(Math.exp(4));
+    System.out.println("exponent of 5 -1: ");
+    System.out.println(Math.expm1(5));
+    System.out.println("rounded value to nearest integer of 5.987: ");
+    System.out.println(Math.floor(5.987));
+    System.out.println("Natural log of 17: ");
+    System.out.println(Math.log(17));
+    System.out.println("base 10 log 17: ");
+    System.out.println(Math.log10(17));
+    System.out.println("value of 3 raised to power of 4: ");
+    System.out.println(Math.pow(3,4));
+  }
+}
