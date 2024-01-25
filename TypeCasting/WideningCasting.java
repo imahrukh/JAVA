c12a0f1ffdb473cc5acba7d86694462f0357064f
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-25 20:39:35 +0500

    widening casting

---
+++ TypeCasting/WideningCasting.java
@@ -0,0 +1,10 @@
+//Widening Casting: conversion from smaller to larger type 
+public class WideningCasting {
+  public static void main(String[] args) {
+    int myInt = 9;
+    double myDouble = myInt; // Automatic casting: int to double
+
+    System.out.println(myInt);      // Outputs 9
+    System.out.println(myDouble);   // Outputs 9.0
+  }
+}
