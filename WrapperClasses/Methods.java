18bf7ee50971ef76846932352b77cc6868f6b107
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-27 10:45:08 +0500

    methods with objects in java

---
+++ WrapperClasses/Methods.java
@@ -0,0 +1,13 @@
+public class Methods { 
+  public static void main(String[] args) { 
+    Integer myInt = 7; 
+    Double myDouble = 2.99; 
+    Character myChar = 'A';
+    //String myString ="Hello";
+    //System.out.printValue(myChar.intValue());
+    System.out.println(myInt.intValue());
+    System.out.println(myDouble.intValue());
+    System.out.println(myDouble.doubleValue());
+    System.out.println(myChar.charValue());
+  }
+}
