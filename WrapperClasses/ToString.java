18ad01bc34c22f10bf4bec776efbc65bdb3db4c2
Mah Rukh <111651927+imahrukh@users.noreply.github.com>
2024-01-27 10:48:07 +0500

    conversion to strings

---
+++ WrapperClasses/ToString.java
@@ -0,0 +1,8 @@
+public class ToString { 
+  public static void main(String[] args) { 
+    Integer myInt = 100; 
+    String myString = myInt.toString();
+    System.out.println("String"+ myString);
+    System.out.println("Size: "+myString.length());
+  }
+}
